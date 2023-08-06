2e758c784e5b2c3c0d03ee3aa1504d81022f8ab4
sunzha <140566489+sunzha@users.noreply.github.com>
2023-08-07 06:48:45 +1000

    update to include precedence

--- convert_postifix.py
+++ convert_postifix.py
@@ -1,17 +1,18 @@
 # add documentation here
 from data_structures.stack_adt import ArrayStack
 
-operators = ["+", "-"]
+operators = {"+": 1, "-": 1, "*": 2, "/": 2} # dictionary containing the operators and their ordering
 
 def convert_to_postifx(infix_expression: str) -> str:
     """ Converts an infix expression to postifix expression
     
     :Input:
-        infix_expression (str): The expression in infix form
-                        i.e. "a+b+c*d"
+        infix_expression (str): The expression in infix notation
+                        i.e. "a + b + c * d"
     
     :Returns:
-        str: The expression in postfix form
+        str: The expression in postfix notation
+                        i.e. "a b + c d * +"
     
     :Complexity: O(n) where n is the length of the infix expression
     """
--- test_convert_postfix.py
+++ test_convert_postfix.py
@@ -5,5 +5,8 @@ class test_convert_postifx(unittest.TestCase):
     def test_simple(self):
         self.assertEqual(convert_to_postifx("a + b + c"), "a b + c +")
 
+    def test_complex(self):
+        self.assertEqual(convert_to_postifx("a + b + c * d"), "a b + c d * +")
+
 if __name__ == "__main__":
     unittest.main()
